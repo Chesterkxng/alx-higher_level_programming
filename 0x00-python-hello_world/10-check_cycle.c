e555a7d2b6b022a1ecf15d290f0ce79c0280068d
Chesterkxng <cgoita00@gmail.com>
2023-10-02 13:34:05 -0700

    turtle and hare

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * check_cycle - check for a cycle
+ * @list: list
+ * Return: 0 if any cycle
+ * 1 othewise
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *turtle, *hare, *head;
+
+	head = list;
+	turtle = head;
+	hare = head;
+
+	if (!list || !turtle->next || !turtle->next->next)
+		return (0);
+	do {
+		turtle = turtle->next;
+		if (hare && hare->next)
+			hare = fast->next->next;
+	} while (hare != turtle && hare);
+	if (!hare)
+		return (0);
+	return (1);
+}
