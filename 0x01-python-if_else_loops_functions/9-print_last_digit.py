b0da36539a684ce2290640bc7b83e25bc5db823d
Chesterkxng <cgoita00@gmail.com>
2023-10-03 12:51:27 -0700

    checking case the repr fails

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     last_digit = repr(number)[-1]
-    print("{}".format(last_digit), end="")
+    if last_digit:
+        print("{}".format(last_digit), end="")
     return (int(last_digit))
