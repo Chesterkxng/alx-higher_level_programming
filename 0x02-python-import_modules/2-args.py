86654d7d2dbcf6c380771c1a49209edbdd39b476
Chesterkxng <cgoita00@gmail.com>
2023-10-05 18:16:22 +0100

    edge case of 1

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-if __name__ ==  "__main__":
+if __name__ == "__main__":
     from sys import argv
     n = len(argv)
     if n == 1:
         print("{} argument.".format(n - 1))
+    elif n == 2:
+        print("{} argument:".format(n - 1))
+        print("{}: {}".format(n - 1, argv[n - 1]))
     else:
         print("{} arguments:".format(n - 1))
         for i in range(1, n):
