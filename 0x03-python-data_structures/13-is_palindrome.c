cad37488534caf6c9190fd3c3fa22d8660dd1194
Chesterkxng <cgoita00@gmail.com>
2023-10-07 17:32:59 +0100

    no malloc need

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -10,29 +10,23 @@ int is_palindrome(listint_t **head)
 {
 	int length = 0, i = 0, j;
 	listint_t *cursor = *head;
-	int *array;
+	int array[64] = {0};
 
 	if (*head == NULL)
 		return (1);
-	array = malloc(sizeof(int) * 1024);
-	if (!array)
-		return (0);
-	cursor = *head;
 	while (cursor)
 	{
-		array[i] = cursor->n;
 		length++;
-		cursor = cursor->next;
+		array[i] = cursor->n;
+		cursor =  cursor->next;
 		i++;
 	}
 	for (j = 0; j < length / 2; j++)
 	{
 		if (array[j] != array[length - j - 1])
 		{
-			free(array);
 			return (0);
 		}
 	}
-	free(array);
 	return (1);
 }
