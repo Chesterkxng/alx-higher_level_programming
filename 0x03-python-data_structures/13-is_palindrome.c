e0da91dc284ba44c5bb86df0fb0bc948dfb39fb9
Chesterkxng <cgoita00@gmail.com>
2023-10-07 17:40:50 +0100

    restore version

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,6 +1,4 @@
 #include "lists.h"
-#include <stddef.h>
-#include <stdlib.h>
 /**
  * is_palindrome - a function that check
  * @head: listint_t
@@ -10,23 +8,33 @@ int is_palindrome(listint_t **head)
 {
 	size_t length = 0, i = 0, j;
 	listint_t *cursor = *head;
-	int array[64] = {0};
+	int *array;
 
 	if (*head == NULL)
 		return (1);
 	while (cursor)
 	{
 		length++;
+		cursor = cursor->next;
+	}
+	array = malloc(sizeof(int) * length);
+	if (!array)
+		return (0);
+	cursor = *head;
+	while (cursor)
+	{
 		array[i] = cursor->n;
-		cursor =  cursor->next;
+		cursor = cursor->next;
 		i++;
 	}
 	for (j = 0; j < length / 2; j++)
 	{
 		if (array[j] != array[length - j - 1])
 		{
+			free(array);
 			return (0);
 		}
 	}
+	free(array);
 	return (1);
 }
