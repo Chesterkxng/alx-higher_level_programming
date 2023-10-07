e08a631918f315217b007ccac27315ffdf238088
Chesterkxng <cgoita00@gmail.com>
2023-10-07 17:25:52 +0100

    buff size default

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -14,7 +14,7 @@ int is_palindrome(listint_t **head)
 
 	if (*head == NULL)
 		return (1);
-	array = malloc(sizeof(int) * length);
+	array = malloc(sizeof(int) * 1024);
 	if (!array)
 		return (0);
 	cursor = *head;
