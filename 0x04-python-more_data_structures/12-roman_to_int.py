a6bb7107374464c7fa5fa2efb5acff2df466de31
Chesterkxng <cgoita00@gmail.com>
2023-10-11 20:45:09 +0100

    edge case correction

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,8 +2,8 @@
 def roman_to_int(roman_string):
     key_map = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     conv_value = 0
-    if not roman_string:
-        return (None)
+    if not isinstance(roman_string, str) or roman_string is None:
+        return (0)
     for i in range(len(roman_string) - 1):
         if key_map[roman_string[i]] >= key_map[roman_string[i + 1]]:
             conv_value += key_map[roman_string[i]]
