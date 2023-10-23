62aef665c7de0f36c16dfb81c0486a25b1d07c8a
Chesterkxng <cgoita00@gmail.com>
2023-10-23 15:13:13 +0100

    safe print integer

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    try:
+        num_value = int(value)
+        print("{:d}".format(num_value))
+        return (True)
+    except ValueError:
+        return (False)
