b0980678fe73374cb7416c410b308ef14f966c63
Chesterkxng <cgoita00@gmail.com>
2023-10-24 15:14:22 +0100

    area of the square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+""" module that defines Square class
+the Square Class represent a Square by his size
+Classes:
+       Square: A class manipulating squares objects
+"""
+
+
+class Square:
+    """
+    A class manipulating square object
+    Attributes:
+    __size: represent the size of the square
+    Methods:
+    __init__(self, size):
+             initializing the square size
+    area(self):
+             return the value of the calculated area
+    """
+    def __init__(self, size=0):
+        """
+        Validate and Initialize the square instance with the size.
+        Args
+        ________
+        size: int
+             the size of the square
+        """
+        if isinstance(size, int):
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """
+        a function returns the computed area of the square
+        Return
+        ------------
+        int:
+           area of the square
+        """
+        return (self.__size ** 2)
