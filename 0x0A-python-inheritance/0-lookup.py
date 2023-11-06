afe0fa6183bfef7d2d6fc1a929dcecdedfa0119c
Chesterkxng <cgoita00@gmail.com>
2023-11-06 11:46:22 +0000

    task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module is used to returns the list of avalaible
+attributes and methods of an object
+"""
+
+
+def lookup(obj):
+    """
+    returning  the list of object and attributes
+    """
+    return (dir(obj))
