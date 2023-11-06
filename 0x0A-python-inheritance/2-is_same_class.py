6df5ff5ebbb5408b9d7c0bf15b865f564a312cd4
Chesterkxng <cgoita00@gmail.com>
2023-11-06 14:08:30 +0000

    task 3

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+module to test the object classes
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    function that return true or false
+    on classes comparassion
+    """
+    return type(obj) is a_class
