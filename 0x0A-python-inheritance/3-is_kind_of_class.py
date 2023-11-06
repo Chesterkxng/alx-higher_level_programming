63dd0fee29cf84c589fdaedfca0d6013ccd5d66a
Chesterkxng <cgoita00@gmail.com>
2023-11-06 14:38:03 +0000

    task 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+module to test the object classes
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    function that return true or false
+    on classes comparassion
+    """
+    return isinstance(obj, a_class)
