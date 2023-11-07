c7e95d819167a0ac7bb982d88140d5a57a049857
Chesterkxng <cgoita00@gmail.com>
2023-11-07 19:13:44 +0000

    fix pystyle

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,12 +7,13 @@ import sys
 
 
 if __name__ == "__main__":
-    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+    load_from_json_file = __import__('6-load_from_json_file')\
+        .load_from_json_file
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
     try:
         list_ = load_from_json_file("add_item.json")
-    except:
+    except Exception:
         list_ = []
     for element in sys.argv[1:]:
         list_.append(element)
