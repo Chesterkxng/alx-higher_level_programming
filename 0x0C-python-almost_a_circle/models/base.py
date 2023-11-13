fb5d22b718879a511ff3aa24c7fb71e1023ddd83
Chesterkxng <cgoita00@gmail.com>
2023-11-13 18:23:58 +0000

    json string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,7 +18,7 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
-            @staticmethod
+    @staticmethod
     def to_json_string(list_dictionaries):
         """ Json string repr """
         if list_dictionaries is None:
