81b350a34b57efea8b3f0b107475726791da6695
Chesterkxng <cgoita00@gmail.com>
2023-12-12 12:30:39 +0000

    7 to 10

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,2 @@
+-- insert a row in a table
+INSERT IGNORE INTO first_table (id, name) VALUES (89, 'Best School');
---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,2 @@
+-- count the record in a database with specific ID
+SELECT COUNT(*) FROM first_table WHERE id = 89;
