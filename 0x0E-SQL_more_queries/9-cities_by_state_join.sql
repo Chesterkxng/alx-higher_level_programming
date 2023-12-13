f59e361bf66563aaf9923214df939b1be7098868
Chesterkxng <cgoita00@gmail.com>
2023-12-13 19:29:33 +0000

    uppercase

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- a script that lists all cities contained in the database
-SELECT cities.id, cities.name, states.name
+SELECT cities.id AS id, cities.name AS name, states.name AS name
 FROM cities
 INNER JOIN states
 ON cities.state_id = states.id;
