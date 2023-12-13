d78dc3d04280492981ca9336ffb0bb300af0ce9c
Chesterkxng <cgoita00@gmail.com>
2023-12-13 19:16:41 +0000

    left join

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- a script that lists all cities contained in the database
 SELECT cities.id, cities.name, states.name
-FROM cities INNER JOIN states
+FROM cities LEFT JOIN states
 ON cities.state_id = states.id;
 ORDER BY cities.id ASC;
