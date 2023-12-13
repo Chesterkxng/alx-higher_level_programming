823eacb265b8a50e250aa17a5488963fb55ac03e
Chesterkxng <cgoita00@gmail.com>
2023-12-13 19:34:14 +0000

    salete de ;

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- a script that lists all cities contained in the database
-SELECT cities.id AS id, cities.name AS name, states.name AS name
+SELECT cities.id, cities.name, states.name
 FROM cities
 INNER JOIN states
-ON cities.state_id = states.id;
+ON cities.state_id=states.id
 ORDER BY cities.id ASC;
