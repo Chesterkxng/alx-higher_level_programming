e0228d0c100b396ce80b537339d9107b4f3b4027
Chesterkxng <cgoita00@gmail.com>
2024-01-25 18:01:19 +0000

    wc -l

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL, sends a request to that URL, and displays the
-# size of the body of the response
+# Bash script that takes in a URL, send request then display size of body
 curl -s -w "%{size_download}" -o /dev/null "$1" | { read size; echo "$size"; }
