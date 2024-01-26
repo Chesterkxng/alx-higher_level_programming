b18a5acf9ebb05495731cb9971b275949c1c6515
Chesterkxng <cgoita00@gmail.com>
2024-01-26 12:04:16 +0000

    error

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Error code
+"""
+if __name__ == "__main__":
+    import urllib.request
+    from urllib.error import HTTPError
+    import sys
+    url = sys.argv[1]
+    req = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(req) as response:
+            body = response.read().decode('utf-8')
+            print(body)
+    except HTTPError as e:
+        print("Error code: {}".format(e.code))
