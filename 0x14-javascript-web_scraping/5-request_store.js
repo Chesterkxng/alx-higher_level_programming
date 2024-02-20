fdf3d78fccca8738a9f8a5972e42a938b36fd98d
Chesterkxng <cgoita00@gmail.com>
2024-02-20 16:05:02 +0000

    semistandard fix

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -5,9 +5,9 @@ request(process.argv[2], function (error, response, body) {
   if (error) {
     console.log(error);
   } else if (response.statusCode === 200) {
-    fs.writeFile(process.argv[3], body,'utf-8', error => {
+    fs.writeFile(process.argv[3], body, 'utf-8', error => {
       if (error) console.log(error);
-    })
+    });
   } else {
     console.log('Error code: ' + response.statusCode);
   }
