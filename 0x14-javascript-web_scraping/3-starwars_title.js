cc0ddcc9184f49d22f90dfb3375a9b488bb4be02
Chesterkxng <cgoita00@gmail.com>
2024-02-20 12:24:47 +0000

    js

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const request = require('request');
+request(`https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`,
+  function (error, response, body) {
+    if (error) {
+      console.log(error);
+    } else if (response.statusCode === 200) {
+      const responseJson = JSON.parse(body);
+      console.log(responseJson.title);
+    }
+    else {
+      console.log('Error code: ' + response.statusCode);
+    }
+  });
