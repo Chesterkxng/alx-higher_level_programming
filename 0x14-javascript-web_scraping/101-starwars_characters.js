65f6544b493d37d23102e87337ab2d8dff5a499c
Chesterkxng <cgoita00@gmail.com>
2024-02-20 17:32:11 +0000

    paste previous one

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const request = require('request');
+const url = 'https://swapi-api.alx-tools.com/api/films/';
+request(url + process.argv[2], function (error, response, body) {
+  if (error) console.log(error);
+  else if (response.statusCode === 200) {
+    const characters = JSON.parse(body).characters;
+    for (let i = 0; i < characters.length; i++) {
+      request(characters[i], function (error, response, body) {
+        if (error) console.log(error);
+        else if (response.statusCode === 200) {
+          const character = JSON.parse(body);
+          console.log(character.name);
+        }
+      });
+    }
+  } else console.log('Error code: ' + response.statusCode);
+});
